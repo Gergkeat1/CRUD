4da398b058561e42b798515b8a777db597300a50
Kan <kanz.kingz@gmail.com>
2019-11-14 17:09:56 +0700

    master

--- example/index.js
+++ example/index.js
@@ -1,14 +1,28 @@
 var path = require('path');
 var express = require('express');
 var expressLayouts = require('..');
+var mysql = require('mysql');
+var http = require('http');
 
 var app = express();
+var router = express.Router();
 
+var url = require('url');
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('X-Foo', 'bar');
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('MehdiFilban solved this problem\nYour Header is set NOW\nGOOD LUCK...');
+}).listen(port, () => {
+  console.log('your app is started');
+});
+
+//BodyParser modules
 var bodyParser = require('body-parser')
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
-})); 
+}));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -16,33 +30,95 @@ app.set('layout extractScripts', true)
 app.set('layout extractStyles', true)
 
 app.use(expressLayouts);
+app.use(express.static('public'));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
+
   res.locals = {
     title: 'Test',
     fname: 'fname',
-    lname: 'lname',    
+    lname: 'lname',
   };
-  res.render('view');
+  var conn = mysql.createConnection({
+
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'test'
+  });
+
+  conn.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
+
+  res.render('view', {
+
+  });
+
 });
 
-app.post('/a', function(req, res) {
+app.post('/a', function (req, res) {
+
+  var conn = mysql.createConnection({
+
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'test'
+  });
+
+  conn.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
+
   var fname = req.body.fname,
-      lname = req.body.lname ;
-  res.render('a', {
-    title: 'Test',
-    fname: req.body.fname,
-    lname: req.body.lname,
+    lname = req.body.lname,
+    age = req.body.age,
+    tel = req.body.tel;
+
+  var sql = "INSERT INTO test_profile (fname, lname, age, tel) VALUES ('" + fname + "', '" + lname + "', '" + age + "', '" + tel + "')";
+  conn.query(sql, [fname, lname, age, tel], function (err, data) {
+    if (err) {
+      console.log("Error inserted into db");
+    } else {
+      console.log("Successfully inserted into db");
+    }
+  });
+
+  var sql = "SELECT * FROM test_profile";
+  conn.query(sql, function (err, result) {
+    res.render('a', { result: result });
   });
 });
 
-app.get('/back', function(req, res) {
-  res.render('view');
+app.get('/delete', function (req, res) {
+  var conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'test'
+  });
+  conn.connect(function (err, result) {
+    if (err) throw err;
+    console.log(result);
+    var id = req.query.id;
+    console.log("ID is = " + id);
+    var sql = "DELETE FROM test_profile WHERE id = '" + id + "'";
+    conn.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("Number of records deleted"+ result.affectedRows);
+      var sql = "SELECT * FROM test_profile";
+      conn.query(sql, function (err, result) {
+        res.render('a', { result: result });
+      });
+    });
+  });
+
 });
 
-
-
 var port = 3000;
-app.listen(port, function() {
+app.listen(port, function () {
   console.log('Listening on http://localhost:%s/', port);
 });
