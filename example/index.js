0bde7d537aa17c827f5936173f0dc67c55e64a97
Gergkeat1 <gergkeat@gmail.com>
2019-11-20 15:21:07 +0700

    CRUD

--- example/index.js
+++ example/index.js
@@ -7,17 +7,7 @@ var url = require('url');
 
 var app = express();
 var router = express.Router();
-// --------------------------passport-----------------------------------
-var flash             = require('connect-flash');
-var crypto            = require('crypto');
-var passport          = require('passport');
-var LocalStrategy     = require('passport-local').Strategy;
-// var connection        = require('./lib/dbconn');
-var sess              = require('express-session');
-var Store             = require('express-session').Store;
-var BetterMemoryStore = require(__dirname + '/memory');
-var BetterMemoryStore = require('session-memory-store')(sess);
-// --------------------------passport-----------------------------------
+
 
 
 const server = http.createServer((req, res) => {
@@ -45,111 +35,7 @@ app.set('layout extractStyles', true)
 app.use(expressLayouts);
 app.use(express.static('public'));
 
-//Express sesion
-var store = new BetterMemoryStore({ expires: 60 * 60 * 1000, debug: true });
-app.use(sess({
-   name: 'JSESSION',
-   secret: 'MYSECRETISVERYSECRET',
-   store:  store,
-   resave: true,
-   saveUninitialized: true
-}));
-// Passport
 
-passport.use('local', new LocalStrategy({
-
-  usernameField: 'username',
-
-  passwordField: 'password',
-
-  passReqToCallback: true //passback entire req to call back
-} , function (req, username, password, done){
-
-
-      if(!username || !password ) { return done(null, false, req.flash('message','All fields are required.')); }
-
-      var salt = '7fa73b47df808d36c5fe328546ddef8b9011b2c6';
-
-      connection.query("select * from test_profile where username = ?", [username], function(err, rows){
-
-          console.log(err); console.log(rows);
-
-        if (err) return done(req.flash('message',err));
-
-        if(!rows.length){ return done(null, false, req.flash('message','Invalid username or password.')); }
-
-        salt = salt+''+password;
-
-        var encPassword = crypto.createHash('sha1').update(salt).digest('hex');
-
-
-        var dbPassword  = rows[0].password;
-
-        if(!(dbPassword == encPassword)){
-
-            return done(null, false, req.flash('message','Invalid username or password.'));
-
-         }
-
-        return done(null, rows[0]);
-
-      });
-
-    }
-
-));
-
-// Serialize and deserialize user information
-
-passport.serializeUser(function(user, done){
-
-  done(null, user.id);
-
-});
-
-passport.deserializeUser(function(id, done){
-
-  connection.query("select * from tbl_users where id = "+ id, function (err, rows){
-
-      done(err, rows[0]);
-
-  });
-
-});
-
-//Signin route for GET method
-
-app.get('/signin', function(req, res){
-  res.render('login/index',{'message' :req.flash('message')});
-});
-//Signin route for POST method to authenticate requests
-
-app.post("/signin", passport.authenticate('local', {
-
-  successRedirect: '/profile',
-
-  failureRedirect: '/signin',
-
-  failureFlash: true
-
-}), function(req, res, info){
-
-  res.render('login/index',{'message' :req.flash('message')});
-
-});
-
-//Protect routes with isAutehticated
-
-
-function isAuthenticated(req, res, next) {
-
-  if (req.isAuthenticated())
-
-    return next();
-
-  res.redirect('/signin');
-
-}
 
 //Login
 
